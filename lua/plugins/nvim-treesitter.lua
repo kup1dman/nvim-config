feb33f9062812bb353f0cf2aae49a217ef5db8d5
kup1dman <n.sinckevich1@gmail.com>
2024-07-05 20:39:57 +0300

    Adjust treesitter to look docs

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "ruby", "lua", "javascript", "python" },
+        ensure_installed = { "ruby", "lua", "javascript", "python", "c", "lua", "vim", "vimdoc", "query" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
